2d1b6f503ef3b13d68988c0b88b8eaf1a6008232
trsh01 <agonzaleznoel@gmail.com>
2026-05-12 19:11:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import mplfinance as mpf
 import pandas as pd
 import yfinance as yf
 from flask import Flask, send_file, request
+import talib
 import matplotlib
 matplotlib.use('Agg') # Necessary for non-GUI backend
 
@@ -18,15 +19,19 @@ def chart_data():
     # 1. Load data
     #df = pd.read_csv('data.csv', index_col=0, parse_dates=True)
     df = yf.Ticker('AAPL').history(period='4mo')[['Open', 'High', 'Low', 'Close', 'Volume']]
-    #with open("coin.txt", "r") as archivo:
-    #    coin=archivo.read()
     # 2. Create memory buffer
+    with open("coin.txt", "r") as archivo:
+        coin=archivo.read()
+    RSI_PERIOD=14
+    closes_array = df['Close'].to_numpy()
+    df['rsi']=talib.RSI(closes_array,RSI_PERIOD) 
+    #df.ta.macd(close='close', fast=6, slow=12, signal=5, append=True)   
     memory_file = io.BytesIO()
-    
+   
     # 3. Plot to buffer
-    mpf.plot(df, type='candle', savefig=dict(fname=memory_file, format="png"))
-    #rsi_plot = mpf.make_addplot(df['rsi'], panel=2, color='blue', ylabel='RSI')
-    #mpf.plot(df, type='candle', style='starsandstripes', volume=True, title='CHART', mav=(20, 50), addplot=rsi_plot, panel_ratios=(4, 2, 2), savefig=dict(fname=memory_file, format="png"))
+    #mpf.plot(df, type='candle', savefig=dict(fname=memory_file, format="png"))
+    rsi_plot = mpf.make_addplot(df['rsi'], panel=2, color='blue', ylabel='RSI')
+    mpf.plot(df, type='candle', style='starsandstripes', volume=True, title=coin+' CHART', mav=(20, 50), addplot=rsi_plot, panel_ratios=(4, 2, 2), savefig=dict(fname=memory_file, format="png"))
         
     # 4. Seek to start
     memory_file.seek(0)
