5757c4b5ea1258035e4fbb676d0945b2afd79ed9
trash3d <agonzaleznoe@gmail.com>
2026-05-12 18:16:29 -0300

    yfinance

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import io
 import mplfinance as mpf
 import pandas as pd
+import yfinance as yf
 from flask import Flask, send_file, request
+from flask_frozen import Freezer
 import matplotlib
 matplotlib.use('Agg') # Necessary for non-GUI backend
 
 app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) # '0.0.0.0' expone la app
+#app = Freezer(__name__)
 
 @app.route('/',methods=['GET'])
 def index():
@@ -17,16 +17,16 @@ def index():
 @app.route('/chartdata')
 def chart_data():
     # 1. Load data
-    df = pd.read_csv('data.csv', index_col=0, parse_dates=True)
-    
+    #df = pd.read_csv('data.csv', index_col=0, parse_dates=True)
+    df = yf.Ticker('AAPL').history(period='4mo')[['Open', 'High', 'Low', 'Close', 'Volume']]
     # 2. Create memory buffer
     memory_file = io.BytesIO()
     with open("coin.txt", "r") as archivo:
         coin=archivo.read()
     # 3. Plot to buffer
-    #mpf.plot(df, type='candle', savefig=dict(fname=memory_file, format="png"))
-    rsi_plot = mpf.make_addplot(df['rsi'], panel=2, color='blue', ylabel='RSI')
-    mpf.plot(df, type='candle', style='starsandstripes', volume=True, title=coin+' CHART', mav=(20, 50), addplot=rsi_plot, panel_ratios=(4, 2, 2), savefig=dict(fname=memory_file, format="png"))
+    mpf.plot(df, type='candle', savefig=dict(fname=memory_file, format="png"))
+    #rsi_plot = mpf.make_addplot(df['rsi'], panel=2, color='blue', ylabel='RSI')
+    #mpf.plot(df, type='candle', style='starsandstripes', volume=True, title=coin+' CHART', mav=(20, 50), addplot=rsi_plot, panel_ratios=(4, 2, 2), savefig=dict(fname=memory_file, format="png"))
         
     # 4. Seek to start
     memory_file.seek(0)
@@ -34,7 +34,6 @@ def chart_data():
     # 5. Return as image
     return send_file(memory_file, mimetype='image/png')
 
-@app.get('/chartdata')
-def chart_view():
-    return '<head><meta http-equiv="refresh" content="5"></head><img src="/chartdata" alt="Financial Chart" />'
-
+#if __name__ == '__main__':
+   #app.run(host='0.0.0.0', port=5000) # '0.0.0.0' expone la app
+#   app.freeze()
